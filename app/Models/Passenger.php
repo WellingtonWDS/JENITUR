051ca67e0c6966bf5d42709ed7a7c02c061cbb8d
Ana Luiza Souza Barbosa <analuizas.b13@gmail.com>
2020-03-08 17:40:40 -0300

    Consertando

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Passenger extends Model
 {
-    protected $fillable = ['CPF', 'NOME', 'RG', 'DATANASC', 'DDD1', 'TELEFONE1', 'DDD2', 'TELEFONE2', ''];
+    protected $fillable = ['CPF', 'NOME', 'RG', 'DATANASC', 'DDD1', 'TELEFONE1', 'DDD2', 'TELEFONE2', 'PCRIANCA', 'NOMECRIANCA', 'CPFCRIANCA', 'RGCRIANCA', 'VAGA'];
     protected $table = 'passageiro';
     public $timestamps = false;
 }
